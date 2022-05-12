d555ac0a75799a98623493986009e69b424cda20
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-05-12 10:24:16 +0200

    1.01
    - Skriptlayout angepasst

---
+++ ShowPasswordAge.ps1
@@ -0,0 +1,4 @@
+﻿# Skript zum anzeigen des Passwortalters aller Benutzer
+# Stannek GmbH - v.1.01 - 12.05.2022 -E.Sauerbier
+
+Get-ADUser -filter { Enabled -eq $True } –Properties PwdLastSet | select sAmAccountName, @{name ="pwdLastSet";expression={[datetime]::FromFileTime($_.pwdLastSet)}} | Sort-Object -Property sAmAccountName | FT -AutoSize 
