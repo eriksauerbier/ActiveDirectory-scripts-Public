ae58a0a26e676195f0a0b40b3e66c2af0ab5ae66
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-05-19 15:18:36 +0200

    v.1.0
    erste Fassung des Skripts

---
+++ ShowLastUserLogon.ps1
@@ -0,0 +1,6 @@
+﻿# Dieses Skript zeigt den letzten Anmeldezeitpunkt alle AD-User an
+# Stannek GmbH v.1.0 - 19.05.2022 - E.Sauerbier
+
+Get-ADUser -Filter * -Properties lastLogon,Mail | Select Surname, Name, Enabled, samaccountname, Mail, @{Name="lastLogon";Expression={[datetime]::FromFileTime($_.'lastLogon')}} | Sort-Object lastLogon -Descending | Format-Table
+
+Read-Host "Zum beenden beliebige Taste drücken"
