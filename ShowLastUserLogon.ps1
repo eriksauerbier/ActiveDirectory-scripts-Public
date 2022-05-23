818c60c70d7e68d4031f8347247a29164f75e0ea
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-05-23 13:58:15 +0200

    v.1.2
    - Abfrage für Organisationseinheit hinzugefügt
    - Ausgabe optimiert

--- ShowLastUserLogon.ps1
+++ ShowLastUserLogon.ps1
@@ -1,5 +1,5 @@
 ﻿# Dieses Skript zeigt den letzten Anmeldezeitpunkt von AD-User an
-# Stannek GmbH v.1.1 - 20.05.2022 - E.Sauerbier
+# Stannek GmbH v.1.2 - 23.05.2022 - E.Sauerbier
 
 # Benutzer-Abfrage zum Alter in Tagen
 Write-Host "`nAD-Benutzer Anzeigen die seit X Tagen nicht mehr angemeldet waren."
@@ -8,14 +8,18 @@ $days = Read-Host "X in Tage oder ENTER für alle Benutzer"
 # Abfrage Tag errechnen
 $QueryDay = (Get-Date).addDays(-$days)
 
+# Organisationseinheit erfragen, bei Abbruch wird das gesamte AD abgefragt
+$OrgUnit = Get-ADOrganizationalUnit -Filter * | Select-Object DistinguishedName | Out-GridView -Title "Bitte wünschte Organisationseinheit angeben, für alle User der Domäne Abbrechen-Button drücken" -OutPutMode Single
+
 # Abfrage der AD-User
-$Output = Get-ADUser -Filter {lastLogon -le $QueryDay} -Properties lastLogon,Mail | Select Surname, Name, Enabled, samaccountname, Mail, @{Name="lastLogon";Expression={[datetime]::FromFileTime($_.'lastLogon')}} | Sort-Object lastLogon -Descending | Format-Table
+If ($OrgUnit -eq $Null) {$Output = Get-ADUser -Filter {lastLogon -le $QueryDay} -Properties lastLogon,Mail | Select-Object Surname, GivenName, Enabled, Mail, @{Name="lastLogon";Expression={[datetime]::FromFileTime($_.'lastLogon')}}, samaccountname | Sort-Object lastLogon}
+Else  {$Output = Get-ADUser -SearchBase $OrgUnit.DistinguishedName -SearchScope Subtree -Filter {lastLogon -le $QueryDay} -Properties lastLogon,Mail | Select-Object Surname, GivenName, Enabled, Mail, @{Name="lastLogon";Expression={[datetime]::FromFileTime($_.'lastLogon')}}, samaccountname | Sort-Object lastLogon}
 
 # Bildschirmausgabe leeren
-Clear-Host
+#Clear-Host
 
 # Ergebnis ausgeben
 If ($days -eq "") {"`nIm ActiveDirectory befinden sich folgende Benutzer"}
 Else {Write-Host "`nFolgende AD-Benutzer haben sich seit $days Tagen nicht mehr am ActiveDirectory angemeldet`n"}
-Write-Host ($Output | Format-Table | Out-String)
+$Output | Select-Object Surname, GivenName, Enabled, Mail, lastLogon, samaccountname | Format-Table -AutoSize
 Read-Host "Zum beenden beliebige Taste drücken"
