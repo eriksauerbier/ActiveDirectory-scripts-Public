0ffe6c37df91f8ad927b5efe5703f24731d7233e
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-05-23 13:56:52 +0200

    v.1.1
    - Abfrage für Organisationseinheit eingefügt

--- ShowLastPCLogon.ps1
+++ ShowLastPCLogon.ps1
@@ -1,19 +1,19 @@
 ﻿# Skript zum Anzeigen alter Domänen PC's
-# Stannek GmbH - v.1.0 - 20.05.2022 -E.Sauerbier
+# Stannek GmbH - v.1.1 - 23.05.2022 -E.Sauerbier
 
 # Benutzer-Abfrage zum Alter in Tagen
 Write-Host "`nGeräte Anzeigen die seit X Tagen nicht mehr angemeldet waren."
 $Days = Read-Host "X in Tage oder ENTER für alle Geräte"
 
-# Organisationseinheit erfragen
-$OrgUnit = Get-ADOrganizationalUnit -Filter * | Select-Object DistinguishedName | Out-GridView -Title "Bitte wünschte Organisationseinheit angeben, Abbrechen für alle PCs" -PassThru
-If ($OrgUnit -eq $Null) {$OrgUnit = "*"}
-
 # Abfrage Tag errechnen
 $QueryDay = (Get-Date).addDays(-$Days)
 
+# Organisationseinheit erfragen, bei Abbruch wird das gesamte AD abgefragt
+$OrgUnit = Get-ADOrganizationalUnit -Filter * | Select-Object DistinguishedName | Out-GridView -Title "Bitte wünschte Organisationseinheit angeben, für alle PCs der Domäne Abbrechen-Button drücken" -OutPutMode Single
+
 # Abfrage der AD-Computer
-$Output = Get-ADComputer -SearchBase $OrgUnit.DistinguishedName -Property LastLogonDate -Filter {lastLogonDate -lt $QueryDay} |Select-Object DNSHostName, LastLogonDate, DistinguishedName | Sort-Object LastLogonDate
+If ($OrgUnit -eq $Null) {$OrgUnit = $Output = Get-ADComputer -Property LastLogonDate -Filter {lastLogonDate -lt $QueryDay} |Select-Object DNSHostName, LastLogonDate, DistinguishedName | Sort-Object LastLogonDate}
+Else  {$Output = Get-ADComputer -SearchBase $OrgUnit.DistinguishedName -SearchScope Subtree -Property LastLogonDate -Filter {lastLogonDate -lt $QueryDay} |Select-Object DNSHostName, LastLogonDate, DistinguishedName | Sort-Object LastLogonDate}
 
 # Bildschirmausgabe leeren
 Clear-Host
