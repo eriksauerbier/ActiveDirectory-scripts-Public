eb30e30dd566d292ca464a5ce056ecdad5c8bc33
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-05-23 15:48:35 +0200

    v.1.0
    erste Version

---
+++ ShowADUserCreationTime.ps1
@@ -0,0 +1,11 @@
+﻿# Dieses Skript zeigt das Erstelldatum von AD-Usern an
+# Stannek GmbH v.1.0 - 23.05.2022 - E.Sauerbier
+
+# Organisationseinheit auslesen
+$OrgUnit = Get-ADOrganizationalUnit -Filter * | Select-Object DistinguishedName | Out-GridView -Title "Bitte wünschte Organisationseinheit angeben, für alle User der Domäne Abbrechen-Button drücken" -OutPutMode Single
+
+# AD-User auslesen
+$Output = Get-ADUser -SearchBase $OrgUnit.DistinguishedName -SearchScope Subtree -Filter * -Properties whenCreated | Sort-Object whenCreated
+
+Write-Host ($Output | Select-Object Name, whenCreated | Format-Table | Out-String)
+Read-Host "Zum beenden beliebige Taste drücken"
