b6c5ced3f148544c05eccb01bb43153757bcaa8c
Erik Sauerbier <erik.sauerbier@mailbox.org>
2023-02-08 14:37:45 +0100

    v.1.02
    - Skriptbeschreibung angepasst
    - Sortierung für Ergebnis optimiert
    - Wartedialog eingefügt

--- ShowPasswordAge.ps1
+++ ShowPasswordAge.ps1
@@ -1,4 +1,7 @@
 ﻿# Skript zum anzeigen des Passwortalters aller Benutzer
-# Stannek GmbH - v.1.01 - 12.05.2022 -E.Sauerbier
+# Stannek GmbH - v.1.02 - 08.02.2023 -E.Sauerbier
 
-Get-ADUser -filter { Enabled -eq $True } –Properties PwdLastSet | select sAmAccountName, @{name ="pwdLastSet";expression={[datetime]::FromFileTime($_.pwdLastSet)}} | Sort-Object -Property sAmAccountName | FT -AutoSize 
+# Passwortalter auslesen
+Get-ADUser -filter { Enabled -eq $True } –Properties PwdLastSet | Select-Object sAmAccountName, @{name ="pwdLastSet";expression={[datetime]::FromFileTime($_.pwdLastSet)}} | Sort-Object -Property PwdLastSet,sAmAccountName | FT -AutoSize 
+
+Read-Host "Zum beenden beliebige Taste drücken"
