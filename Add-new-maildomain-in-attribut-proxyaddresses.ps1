f848257354f04052fe24d7531dd71b9274c5bbc8
eriksauerbier <erik.sauerbier@mailbox.org>
2024-02-07 14:59:57 +0100

    v.1.1
    - Optimierung für das Setzen der Hauptadresse
    - Abfrage von Vor- und Nachname
    - Optionale Zeile, falls sich das Format der Adresse ändert

--- Add-new-maildomain-in-attribut-proxyaddresses.ps1
+++ Add-new-maildomain-in-attribut-proxyaddresses.ps1
@@ -1,5 +1,5 @@
 ﻿# Skript zum hinzufügen einer weiteren E-Mail Domain im Attribut "proxyaddresses"
-# Stannek GmbH - v.1.01 - 23.11.2022 - E.Sauerbier
+# Stannek GmbH - v.1.1 - 07.02.2024 - E.Sauerbier
 
 # Parameter
 $newdomain = "@newdomain.com"
@@ -12,15 +12,17 @@ $OrgUnit = Get-ADOrganizationalUnit -Filter * | Select-Object DistinguishedName 
 If ($OrgUnit -eq $Null) {Break}
 
 # User auslesen
-$Users = Get-ADUser -Filter * -SearchBase $OrgUnit.DistinguishedName -Properties proxyAddresses,mail | Select SamAccountName,proxyAddresses,mail
+$Users = Get-ADUser -Filter * -SearchBase $OrgUnit.DistinguishedName -Properties proxyAddresses,mail,Surname,givenName | Select SamAccountName,proxyAddresses,mail,Surname,givenName
 
 # weitere E-Mail-Adresse hinzufügen
 foreach ($User in $Users) {
   # Hinweis für aktuellen Benutzer ausgegben
   Write-Host "Bearbeite Nutzer: "$User.SamAccountName
   # Hauptadresse setzen, falls dies noch nicht geschehen
-  If ($user.proxyaddresses -cnotlike "SMTP*") {
+  If ($user.proxyaddresses -notcontains "SMTP:$($User.mail)") {
   Set-ADUser -Identity $User.SamAccountName -Add @{proxyAddresses="SMTP:"+$User.mail}}
   # weitere Adresse mit neuer Domäne hinzufügen
   Set-ADUser -Identity $User.SamAccountName -Add @{proxyAddresses="smtp:"+$User.mail.Split("@")[0]+$newdomain}
+  # Falls die neue Adresse abweichend ist. Auf Umlaute achten!
+  #Set-ADUser -Identity $User.SamAccountName -Add @{proxyAddresses="smtp:"+$User.givenName+"."+$User.Surname+$newdomain}
   } 
