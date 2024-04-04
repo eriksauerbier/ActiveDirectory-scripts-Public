1ee6db0a7652fc6cf9d78c4a0becc0bd503c6cd4
eriksauerbier <erik.sauerbier@mailbox.org>
2024-04-04 17:04:15 +0200

    v.1.0
    - First Release

---
+++ Change-DisplayName-per-OU.ps1
@@ -0,0 +1,23 @@
+﻿# Skript zum Ändern des Anzeigenamens und bei Bedarf Firmenname + Homepage
+# Stannek GmbH - v.1.0 - 03.04.2024 - E.Sauerbier
+
+# Parameter
+$NewCompanyName = "Now Kanzlei"
+$NewDisplayNameSuffix = " - Now Kanzlei"
+$NewHomePage = "www.now-kanzlei.de"
+
+# Organisationseinheit erfragen
+$OrgUnit = Get-ADOrganizationalUnit -Filter * | Select-Object DistinguishedName | Out-GridView -Title "Bitte wünschte Organisationseinheit angeben, in der der Anzeigename geändert werden sollen" -OutPutMode Single
+
+# Skript abbrechen wenn keine OU ausgewählt wurde
+If ($OrgUnit -eq $Null) {Break}
+
+# User auslesen
+$Users = Get-ADUser -Filter * -SearchBase $OrgUnit.DistinguishedName -Properties SamAccountName,sn,givenName,wWWHomePage,Company,displayName | Select SamAccountName,sn,givenName,wWWHomePage,Company,displayName
+
+# Userdaten aktualisieren
+foreach ($User in $Users) {
+  # Hinweis für aktuellen Benutzer ausgegben
+  Write-Host "Bearbeite Nutzer: "$User.SamAccountName
+  Set-ADUser -Identity $User.SamAccountName -Company $NewCompanyName -DisplayName $($User.sn + $NewDisplayNameSuffix) -HomePage $NewHomePage
+  }
