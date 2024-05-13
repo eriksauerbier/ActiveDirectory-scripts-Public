2ef4f02d0e909f30e4347ed9404f3fb88311dc04
Erik Sauerbier <erik.sauerbier@mailbox.org>
2024-05-13 15:37:04 +0200

    v.1.0.1
    - Fehler in Featureprüfung behoben

---
+++ Install-AD-CA.ps1
@@ -0,0 +1,23 @@
+﻿# Skript zum Installieren einer ActiveDirectory Zertifizierungsstelle
+# Stannek GmbH - v.1.0.1 - 13.05.2024 - E.Sauerbier
+
+# Parameter
+$ParamCAVPU = 7 # Gueltigkeitsdauer der CA-Zertifikate
+$ParamVPU = 5 # Gueltigkeitsdauer der Zertifikate
+
+# pruefen ob die Zertifizierungsstelle bereits installiert ist
+If ($(Get-WindowsFeature -Name ADCS-Cert-Authority).InstallState -ne "Installed") {
+
+    # Windows Feature installieren
+    Install-WindowsFeature -Name ADCS-Cert-Authority -IncludeManagementTools
+
+    # Zertifizierungsstelle einrichten
+    Install-AdcsCertificationAuthority -CAType EnterpriseRootCa -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" -KeyLength 4096 -HashAlgorithmName SHA256 -ValidityPeriod Years -ValidityPeriodUnits $ParamCAVPU -Force
+
+    # Gueltigkeitsdauer der Zertifikate verlaengern
+    certutil -setreg ca\ValidityPeriodUnits $ParamVPU
+    
+    # Dienst neustartet
+    Restart-Service -Name CertSvc -Force
+    }
+Else {Throw "Zertifizierungsstelle bereits installiert"}
