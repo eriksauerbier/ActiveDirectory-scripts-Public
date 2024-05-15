6ccf095aa4474eabfd9b9ff1704eb482917b5ef6
Erik Sauerbier <erik.sauerbier@mailbox.org>
2024-05-15 13:03:16 +0200

    v.1.1
    - Wartezeit eingefügt
    - Altlasten ggf. entfernen

--- Install-AD-CA.ps1
+++ Install-AD-CA.ps1
@@ -1,5 +1,5 @@
 ﻿# Skript zum Installieren einer ActiveDirectory Zertifizierungsstelle
-# Stannek GmbH - v.1.0.2 - 14.05.2024 - E.Sauerbier
+# Stannek GmbH - v.1.1 - 15.05.2024 - E.Sauerbier
 
 # Parameter
 $ParamCAVPU = 7 # Gueltigkeitsdauer der CA-Zertifikate
@@ -7,17 +7,26 @@ $ParamVPU = 5 # Gueltigkeitsdauer der Zertifikate
 
 # pruefen ob die Zertifizierungsstelle bereits installiert ist
 If ($(Get-WindowsFeature -Name ADCS-Cert-Authority).InstallState -ne "Installed") {
-
+  
     # Windows Feature installieren
     Install-WindowsFeature -Name ADCS-Cert-Authority -IncludeManagementTools
 
     # Zertifizierungsstelle einrichten
     Install-AdcsCertificationAuthority -CAType EnterpriseRootCa -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" -KeyLength 4096 -HashAlgorithmName SHA512 -ValidityPeriod Years -ValidityPeriodUnits $ParamCAVPU -Force
 
+    # kurzen moment warten
+    Start-Sleep -Seconds 30
+
     # Gueltigkeitsdauer der Zertifikate verlaengern
     certutil -setreg ca\ValidityPeriodUnits $ParamVPU
     
     # Dienst neustartet
     Restart-Service -Name CertSvc -Force
+
+    # falls vorhanden Altlasten entfernen
+    If (Test-Path -Path 'HKLM:/Software/Microsoft/Cryptography/Services/NTDS/SystemCertificates/My/Certificates') {Remove-Item -Path 'HKLM:/Software/Microsoft/Cryptography/Services/NTDS' -Recurse -Force}
+
+    # Richtlinien aktualisieren
+    gpupdate /force
     }
 Else {Throw "Zertifizierungsstelle bereits installiert"}
