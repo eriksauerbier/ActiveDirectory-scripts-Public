ab49e762d8239d0ad45a5c243d5b17160968850e
Erik Sauerbier <erik.sauerbier@mailbox.org>
2024-11-13 06:35:33 +0100

    v.1.0
    - Firstrelease

---
+++ Rename-ADGroup.ps1
@@ -0,0 +1,16 @@
+﻿# Skript zum umbenennen von AD Gruppen
+# Stannek GmbH  v.1.0 - 13.11.2024 - E.Sauerbier
+
+# Parameter
+$OldGroupName = 'Alte Gruppe'
+$NewGroupName =  'Neue Gruppe' 
+$NewDescrip = "Gruppe"
+
+# checken ob Gruppe vorhanden
+try {Get-ADGroup -Identity $OldGroupName}
+catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
+{Write-Host "Gruppe nicht vorhanden";break}
+
+# Gruppe umbenennen
+Get-ADGroup -Identity $OldGroupName | Rename-ADObject -NewName $NewGroupName
+Get-ADGroup -Identity $OldGroupName | Set-ADGroup -SamAccountName $NewGroupName -DisplayName $NewGroupName -Description $NewDescrip
