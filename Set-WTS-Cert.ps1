80d38f448299c3646c55b7daddab9e92956692e6
eriksauerbier <erik.sauerbier@mailbox.org>
2024-12-09 13:47:24 +0100

    Delete Set-WTS-Cert.ps1

--- Set-WTS-Cert.ps1
+++
@@ -1,8 +0,0 @@
-$certs=get-childitem -path cert:/LocalMachine/My
-foreach ($c in $certs) {   
-    if ($c.Subject.IndexOf("CN=*.") -eq 0) {
-        write-host -ForegroundColor Yellow  $c.Subject
-        $t=[string]$c.thumbprint
-        wmic /namespace:\\root\CIMV2\TerminalServices PATH Win32_TSGeneralSetting Set SSLCertificateSHA1Hash=$t 
-    } 
-}
