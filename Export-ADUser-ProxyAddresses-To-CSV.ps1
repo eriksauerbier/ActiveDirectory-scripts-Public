b4b61f65cc0b164ecafc3176c511310d6d1d19d3
eriksauerbier <erik.sauerbier@mailbox.org>
2025-04-04 11:38:22 +0200

    v.1.1
    - es werden nun ungewünschte ProxyAdress Einträge entfernt (x500-Adresse oder interne Domain)

--- Export-ADUser-ProxyAddresses-To-CSV.ps1
+++ Export-ADUser-ProxyAddresses-To-CSV.ps1
@@ -1,9 +1,10 @@
 ﻿## PS-Skript zum Exportieren aller E-Mail-Adressen (Attribut: proxyAddresses) pro User
-## Stannek GmbH v.1.0 - 01.07.2024 - E.Sauerbier
+## Stannek GmbH v.1.1 - 03.04.2025 - E.Sauerbier
 
 # Parameter
 $ADAttribute = "userPrincipalName, proxyAddresses" # Name des AD-Attributs angeben
 $FileOutputName = "ADAttribute-$ADAttribute-"+(Get-Date -Format dd-MM-yyyy)+".csv"
+$internalDomain = "dom.local" # Lokale Domain
 
 # ActiveDirectory PS-Modul laden
 Import-Module ActiveDirectory
@@ -18,15 +19,23 @@ $OrgUnit = Get-ADOrganizationalUnit -Filter * | Select-Object Name, Distinguishe
 
 # Attribut "ProxyAddresses" auslesen
 
-$ADObjects = Get-ADObject -SearchBase $OrgUnit.DistinguishedName -Filter * -Properties userPrincipalName, proxyAddresses
+$ADObjects = Get-ADObject -SearchBase $OrgUnit.DistinguishedName -Filter "Mail -like '*@*'" -Properties userPrincipalName, proxyAddresses
 
 # Werte in CSV schreiben und ausgeben
 ForEach ($Object In $ADObjects) {
-  #ForEach ($Attribute in $Object.$ADAttribute) {
-    $Output = $Object.Name+ "," + $Object.userPrincipalName + "," + $Object.proxyAddresses
-    # Zeile ausgeben
-    Write-Host $Output
-    # Wert in CSV schreiben
-    $Output | Out-File $FileOutput -Append
-  #}
-}
+        # Proxy-Adressen nullen
+        $proxyAddresses = $Null
+        # Proxy-Adressen auslesen
+        Foreach ($proxyAddress in $Object.proxyAddresses) {
+         # X500- und interne Domain-Adressen ausschliessen
+         If (($proxyAddress -notlike "x500:/*") -and ($proxyAddress -notlike "*@$internalDomain")) {
+                $proxyAddresses += @($proxyAddress)
+                }
+         }
+       # Ausgabe erstellen
+       $Output = $Object.Name+ "," + $Object.userPrincipalName + "," + $proxyAddresses
+       # Zeile zur Info ausgeben
+       Write-Host $Output
+       # Wert in CSV schreiben
+       $Output | Out-File $FileOutput -Append
+   }
