22651b26e72eb53b67a8a4619b2845806c3d57e2
Erik Sauerbier <erik.sauerbier@mailbox.org>
2025-05-27 12:43:16 +0200

    v.1.2
    - die interne Domain wird nun automatisch ausgelesen
    - Erkennung des Arbeitspfades wurde optimiert

--- Export-ADUser-ProxyAddresses-To-CSV.ps1
+++ Export-ADUser-ProxyAddresses-To-CSV.ps1
@@ -1,24 +1,27 @@
 ﻿## PS-Skript zum Exportieren aller E-Mail-Adressen (Attribut: proxyAddresses) pro User
-## Stannek GmbH v.1.1 - 03.04.2025 - E.Sauerbier
+## Stannek GmbH v.1.2 - 23.05.2025 - E.Sauerbier
 
 # Parameter
 $ADAttribute = "userPrincipalName, proxyAddresses" # Name des AD-Attributs angeben
 $FileOutputName = "ADAttribute-$ADAttribute-"+(Get-Date -Format dd-MM-yyyy)+".csv"
-$internalDomain = "dom.local" # Lokale Domain
+$WorkPath = If($PSISE){Split-Path -Path $psISE.CurrentFile.FullPath}else{Split-Path -Path $MyInvocation.MyCommand.Path}
+IF ($Null -eq $WorkPath) {$WorkPath = Split-Path $psEditor.GetEditorContext().CurrentFile.Path} #Falls Skript in VS Code ausgefuehrt wird
+
 
 # ActiveDirectory PS-Modul laden
 Import-Module ActiveDirectory
 
-# Skriptpfad auslesen und Ausgabedatei erzeugen
-$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
-$FileOutput = $PSScriptRoot + "\" + $FileOutputName
+# lokale AD-Domain auslesen
+$internalDomain = Get-ADDomain | Select-Object -ExpandProperty DNSRoot
+
+# Ausgabedatei erzeugen
+$FileOutput = Join-Path -Path $WorkPath -ChildPath $FileOutputName
 "Name,$ADAttribute" | Out-File $FileOutput
 
 # Organisationseinheit erfragen
 $OrgUnit = Get-ADOrganizationalUnit -Filter * | Select-Object Name, DistinguishedName | Out-GridView -Title "Bitte wünschte Organisationseinheit angeben" -OutPutMode Single
 
 # Attribut "ProxyAddresses" auslesen
-
 $ADObjects = Get-ADObject -SearchBase $OrgUnit.DistinguishedName -Filter "Mail -like '*@*'" -Properties userPrincipalName, proxyAddresses
 
 # Werte in CSV schreiben und ausgeben
